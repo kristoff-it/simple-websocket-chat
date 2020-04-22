898421da258da8d3e4057d8692b628ead5312664
Loris Cro <kappaloris@gmail.com>
2020-04-22 21:21:33 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # simple-websocket-chat
-Simple websocket chat demo that uses Redis Pub/Sub for horizontal scaling
+Simple websocket chat demo that uses Redis Pub/Sub for horizontal scaling.
+
+## Requirements
+- A running Redis instance on the default port.
+
+## Usage
+Install the dependences (`npm install`) and then just run the server process as follows:
+
+`$ node server.js 8080`
+
+The server will prompt you to connect to a specific URL that will provide the client HTML
+interface. By modifying the URL you can authenticate as different users.
+
+Try to spin up multiple instances of the chat server and look how all clients will still
+be able to communicate even if connected to different processes.
+
---
+++ server.js
@@ -0,0 +1,72 @@
+const { Multiplexer } = require("redis-mpx");
+const redis = require('redis');
+const WebSocket = require('ws');
+
+// Just used to server the static client.html file:
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+// Create a Redis Pub/Sub multiplexer and a Redis connection
+// to publish chat messages over.
+let mpx = new Multiplexer("localhost:6379");
+let publish = redis.createClient({retry_strategy: () => 100});
+const MAIN_CHANNEL = "simple-chat-main-channel";
+
+// Start the websocket server
+const wss = new WebSocket.Server({ noServer: true });
+
+// Start the basic http server that will return the client html page
+const server = http.createServer(function (req, res) {
+	fs.readFile("client.html", function (err,data) {
+		res.writeHead(200);
+		res.end(data);
+	});
+});
+
+// We are attaching the websocket server to the main http server
+// manually because we want to store inside the websocket object
+// the username that the user declared when connecting.
+server.on("upgrade", function(request, socket, head) {
+	const connUrl = new URL(request.url, "http://localhost");
+	if (connUrl.pathname === '/ws') {
+		wss.handleUpgrade(request, socket, head, function done(ws) {
+			ws.username = connUrl.searchParams.get("username");
+			console.log(connUrl.searchParams);
+			wss.emit('connection', ws, request);
+		});
+	}
+})
+
+
+wss.on('connection', function connection(ws) {
+	ws.sub = mpx.createPatternSubscription(MAIN_CHANNEL, function (ch, msg){
+		ws.send(msg.toString());
+	});
+
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+		let completeMsg = `${ws.username}: ${message}`;
+		publish.publish(MAIN_CHANNEL, completeMsg);
+	});
+
+	ws.on('close', function () {
+		console.log("[ws] closing");
+		ws.sub.close();
+	});
+
+	ws.send("You are now autenticated as: " + ws.username);
+});
+
+if (process.argv.length != 3) {
+	console.log("usage: node server.js <port>");
+	process.exit();
+}
+
+
+
+console.log(`Open http://localhost:${process.argv[2]}/?username=alice in your browser.`);
+console.log(`Change the username inside the URL to "authenticate" as a different user.`);
+console.log(`\nSpin up multiple instances of this process and see how clients can still all communicate.`);
+
+server.listen(process.argv[2]);
